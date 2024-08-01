c4c445c5073f0a3c97105bb8d4a4815d3d00a549
mario mora <jorgemariomoramolina@gmail.com>
2024-08-01 12:30:12 -0500

    update my code

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,48 +1,70 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing: false
-  });
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(null, title, imageUrl, description, price);
+    product.save();
+    res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  }
-  const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
     });
-  });
 };
 
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updateTitle = req.body.title;
+    const updateImageUrl = req.body.imageUrl;
+    const updatePrice = req.body.price;
+    const updateDescription = req.body.description;
+    const updateProduct = new Product(
+        prodId,
+        updateTitle,
+        updateImageUrl,
+        updateDescription,
+        updatePrice
+    );
+    updateProduct.save();
+    res.redirect('/admin/products')
+}
+
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
+        });
     });
-  });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+}
