1f0b6e2263617402ac08a63f8ed0b3a8e96dd8fb
mario mora <jorgemariomoramolina@gmail.com>
2024-08-22 16:07:45 -0500

    configuring to user

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -18,19 +20,42 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findById(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+//.sync({ force: true })
+    .sync()
     .then(results => {
-        console.log('sequelize connect')
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'jorge', email: 'test@gmai.com' })
+        }
+        return user;
+    })
+    .then(user => {
+        //console.log(user)
+        app.listen(config.port, () => {
+            console.log(config.messague)
+        });
     })
     .catch(err => {
         console.log(err)
     })
-app.listen(config.port, () => {
-    console.log(config.messague)
-});
