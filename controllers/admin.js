a33815523c07467586373ce18e52247eec4ba925
mario mora <jorgemariomoramolina@gmail.com>
2024-08-22 10:49:01 -0500

    updating to products

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,17 +31,21 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if (!product) {
-            return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
+    Product.findByPk(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => {
+            console.log(err)
         });
-    });
 };
 
 
@@ -51,25 +55,35 @@ exports.postEditProduct = (req, res, next) => {
     const updateImageUrl = req.body.imageUrl;
     const updatePrice = req.body.price;
     const updateDescription = req.body.description;
-    const updateProduct = new Product(
-        prodId,
-        updateTitle,
-        updateImageUrl,
-        updateDescription,
-        updatePrice
-    );
-    updateProduct.save();
-    res.redirect('/admin/products')
-}
+    Product.findByPk(prodId)
+        .then(product => {
+            product.title = updateTitle;
+            product.price = updatePrice;
+            product.description = updateDescription;
+            product.imageUrl = updateImageUrl;
+            return product.save();
+        })
+        .then(result => {
+            console.log('UPDATE PRODUCT');
+            res.redirect('admin/products');
+        })
+        .catch(err => {
+            console.log(err)
+        })
+};
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products'
+    Product.findAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => {
+            console.log(err)
         });
-    });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
