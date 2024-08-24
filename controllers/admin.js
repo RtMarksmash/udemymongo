5379abfb41d5a20f4d1ec8fce26280973d9c790e
mario mora <jorgemariomoramolina@gmail.com>
2024-08-23 19:57:55 -0500

    creating to dummy user

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,17 +13,18 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
-        title: title,
-        imageUrl: imageUrl,
-        price: price,
-        description: description,
-    }).then(result => {
-        console.log('product created')
-        res.redirect('/admin/products')
-    }).catch(err => {
-        console.log(err)
-    })
+    req.user.createProduct({
+            title: title,
+            imageUrl: imageUrl,
+            price: price,
+            description: description,
+        })
+        .then(result => {
+            console.log('product created')
+            res.redirect('/admin/products')
+        }).catch(err => {
+            console.log(err)
+        })
 };
 
 exports.getEditProduct = (req, res, next) => {
