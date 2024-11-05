887c623c786ab7531447ed3c89844e84129d706c
mario mora <jorgemariomoramolina@gmail.com>
2024-11-05 16:44:14 -0500

    final commit about orders app

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('67268528b4386ec0ead92958')
         .then(user => {
-            req.user = new User(user.username, user.email, user.cart, user._id);
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -3,10 +3,9 @@ const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 
-
 class User {
     constructor(username, email, cart, id) {
-        this.username = username;
+        this.name = username;
         this.email = email;
         this.cart = cart; // {items: []}
         this._id = id;
@@ -42,24 +41,26 @@ class User {
             .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
     }
 
-
     getCart() {
         const db = getDb();
         const productIds = this.cart.items.map(i => {
             return i.productId;
-        })
-
-        return db.collection('products').find({ _id: { $id: productIds } }).toArray().then(products => {
-            return products.map(p => {
-                return {...p,
-                    quantity: this.cart.items.find(i => {
-                        return i.productId.toString() === p._id.toString();
-                    })
-                }.quantity
-            })
-        })
+        });
+        return db
+            .collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => {
+                return products.map(p => {
+                    return {
+                        ...p,
+                        quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === p._id.toString();
+                        }).quantity
+                    };
+                });
+            });
     }
-
 
     deleteItemFromCart(productId) {
         const updatedCartItems = this.cart.items.filter(item => {
@@ -69,7 +70,35 @@ class User {
         return db
             .collection('users')
             .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: { items: updatedCartItems } } });
+    }
 
+    addOrder() {
+        const db = getDb();
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.name
+                    }
+                };
+                return db.collection('orders').insertOne(order);
+            })
+            .then(result => {
+                this.cart = { items: [] };
+                return db
+                    .collection('users')
+                    .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: { items: [] } } });
+            });
+    }
+
+    getOrders() {
+        const db = getDB();
+        return db
+            .collection('orders')
+            .find({ 'user._id': new ObjectId(this._id) })
+            .toArray();
     }
 
     static findById(userId) {
